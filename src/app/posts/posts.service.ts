6074ee335ee51d1b490a476013a0cb44f179ffce
rickschuurman <rick.schuurman@malmberg.nl>
2019-01-11 15:24:03 +0100

    Finishing edit feature

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -2,19 +2,19 @@ import { Injectable } from "@angular/core";
 import { Subject } from "rxjs";
 import { HttpClient } from "@angular/common/http";
 import { map } from "rxjs/operators";
-
 import { Post } from "./post.model";
+import { Router } from "@angular/router";
 
 @Injectable({ providedIn: "root" })
 export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   getPosts() {
     this.http
-      .get<{ message: string; posts: any }>("http://localhost:3000/api/getposts")
+      .get<{ message: string; posts: any }>("http://localhost:3000/api/posts")
       .pipe(
         map(postData => {
           return postData.posts.map(post => {
@@ -38,21 +38,38 @@ export class PostsService {
 
 
   getPost(id: string) {
-    return { ...this.posts.find(p => p.id === id) };
+    return this.http.get<{_id: string, title: string, content: string}>("http://localhost:3000/api/posts/" + id);
   }
 
 
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
     this.http
-      .post<{ message: string, postId: string }>("http://localhost:3000/api/addposts", post)
+      .post<{ message: string, postId: string }>("http://localhost:3000/api/posts", post)
       .subscribe(responseData => {
         const id = responseData.postId;
         post.id = id;
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
+        this.router.navigate(["/"]);
       });
   }
+
+
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = {id: id, title: title, content: content };
+    this.http
+    .put("http://localhost:3000/api/posts/" + id, post)
+    .subscribe(response => {
+      const updatedPosts = [...this.posts];
+      const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+      updatedPosts[oldPostIndex] = post;
+      this.posts = updatedPosts;
+      this.postsUpdated.next([...this.posts]);
+      this.router.navigate(["/"]);
+    });
+  }
+
 
   deletePost(postId: string) {
     this.http
