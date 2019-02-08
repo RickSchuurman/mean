114944852cd7b4cd7292cfc6738178453f4377b6
rickschuurman <rick.schuurman@malmberg.nl>
2019-02-08 14:25:03 +0100

    Adding image uploads

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,17 +1,48 @@
 const express = require("express");
+const multer = require("multer");
+
 const Post = require("../models/post");
 
 const router = express.Router();
 
-router.post("", (req, res, next) => {
+const MIME_TYPE_MAP = {
+  'image/pgn': 'png',
+  'image/jpeg': 'jpeg',
+  'image/jpg': 'jpg'
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error("Invalid mime type");
+    if (isValid) {
+      error = null;
+    }
+    cb(error, "backend/images");
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLowerCase().split(' ').join('-');
+    const ext = MIME_TYPE_MAP[file.mimetype];
+    cb(null, name + '-' + Date.now() + '.' + ext);
+  }
+});
+
+router.post("", multer({storage: storage}).single("image"), (req, res, next) => {
+  const url = req.protocol + '://' + req.get("host");
   const post = new Post({
     title: req.body.title,
-    content: req.body.content
+    content: req.body.content,
+    imagePath: url + "/images/" + req.file.filename
   });
-  post.save().then(result => {
+  post.save().then(createdPost => {
     res.status(201).json({
       message: "Post added succesfully",
-      postId: result._id
+      post: {
+        id: createdPost._id,
+        title: createdPost.title,
+        content: createdPost.content,
+        imagePath: createdPost.imagePath
+      }
     });
   });
 });
@@ -26,20 +57,26 @@ router.get("", (req, res, next) => {
 });
 
 router.get("/:id", (req, res, next) => {
- Post.findById(req.params.id).then(post => {
-   if (post) {
-     res.status(200).json(post);
-   } else {
-     res.status(404).json({message: 'Post not found!'})
-   }
- })
+  Post.findById(req.params.id).then(post => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({message: 'Post not found!'})
+    }
+  })
 });
 
-router.put("/:id", (req, res, next) => {
+router.put("/:id", multer({storage: storage}).single("image"), (req, res, next) => {
+  let imagePath = req.body.imagePath;
+  if (req.file) {
+    const url = req.protocol + '://' + req.get("host");
+    imagePath = url + "/images/" + req.file.filename
+  }
   const post = new Post({
     _id: req.body.id,
     title: req.body.title,
-    content: req.body.content
+    content: req.body.content,
+    imagePath: imagePath
   });
   Post.updateOne({_id: req.params.id}, post).then(result => {
     console.log(result);
@@ -48,9 +85,9 @@ router.put("/:id", (req, res, next) => {
 });
 
 router.delete("/:id", (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then(result => {
+  Post.deleteOne({_id: req.params.id}).then(result => {
     console.log(result);
-    res.status(200).json({ message: "Post deleted" });
+    res.status(200).json({message: "Post deleted"});
   });
 });
 
